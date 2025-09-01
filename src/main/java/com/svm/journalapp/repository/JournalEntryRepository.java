70a1d33fae8b81267a53b216190f26128680a138
shubhammalve1 <shubhammalve098@gmail.com>
2025-09-01 15:07:53 +0530

    main -> JournalEntryRepository.java interface added

---
+++ src/main/java/com/svm/journalapp/repository/JournalEntryRepository.java
@@ -0,0 +1,8 @@
+package com.svm.journalapp.repository;
+
+import com.svm.journalapp.entities.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+}
