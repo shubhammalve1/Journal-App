aedd68c74be0cc8d1439a3e6b109818372f95527
shubhammalve1 <shubhammalve098@gmail.com>
2025-09-01 15:05:29 +0530

    main -> JournalEntry.java added

---
+++ src/main/java/com/svm/journalapp/entities/JournalEntry.java
@@ -0,0 +1,57 @@
+package com.svm.journalapp.entities;
+
+import lombok.Data;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+// We are using Lombok Library of java to automate getters and setters
+//so we don't need to add it manually.
+
+@Document(collection = "journal_entries")
+@Data
+public class JournalEntry {
+    @Id
+    private ObjectId id;
+
+    @NonNull
+    private String title;
+
+    private String content;
+    private LocalDate date;
+
+//    public ObjectId getId() {
+//        return id;
+//    }
+//
+//    public void setId(ObjectId id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public LocalDate getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(LocalDate date) {
+//        this.date = date;
+//    }
+}
