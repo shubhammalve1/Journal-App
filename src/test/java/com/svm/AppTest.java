9c6578f6222ecb6e1d7d8599187fa52f960ef0c8
shubhammalve1 <shubhammalve098@gmail.com>
2025-09-01 14:39:09 +0530

    Spring Boot with maven Setup is done, everything is working properly

---
+++ src/test/java/com/svm/AppTest.java
@@ -0,0 +1,15 @@
+package com.svm;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
